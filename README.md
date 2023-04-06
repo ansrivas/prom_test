52d658e4c41a58a2bd12a19184d5ab434c793ff0
hengfeiyang <hengfei.yang@gmail.com>
2023-04-06 21:30:49 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Prometheus parse test
+
+core code copy from https://github.com/GreptimeTeam/greptimedb/tree/develop/src/promql
--- examples/test.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let a = "/home/user/Downloads/";
-    let b = a.split('/').take(20).collect::<Vec<&str>>();
-    println!("{:?}", b);
-}
