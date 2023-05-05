185046ac7e5c7fb04ae0a3ad3228a2802f55d49e
Valeriy V. Vorotyntsev <valery.vv@gmail.com>
2023-05-05 04:15:55 +0300

    test: benchmark all the queries used in `locustfile.py` (#50)
    test: benchmark all the queries from `locustfile.py`

--- newpromql/benches/it.rs
+++ newpromql/benches/it.rs
@@ -3,34 +3,53 @@ use std::{
     time::{Duration, UNIX_EPOCH},
 };
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use promql_parser::parser;
 use tokio::runtime::Runtime;
 
 fn bench_promql(c: &mut Criterion) {
-    let query = r#"sum by(stream_type) (delta(zo_storage_files{namespace="ziox-alpha1",organization="default"}[5m]))"#;
-
     let samples_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/../samples");
     let (start, end) = prom_test::load_timestamps(samples_dir).unwrap();
-    // REVIEW: do we want to measure PromQL parsing also?
-    let prom_expr = parser::parse(query).unwrap();
-    let eval_stmt = parser::EvalStmt {
-        expr: prom_expr,
-        start: UNIX_EPOCH.checked_add(Duration::from_secs(start)).unwrap(),
-        end: UNIX_EPOCH.checked_add(Duration::from_secs(end)).unwrap(),
-        interval: Duration::from_secs(15), // step
-        lookback_delta: Duration::from_secs(300),
-    };
-
     let ctx = Arc::new(prom_test::create_context(samples_dir).unwrap());
     let rt = Runtime::new().unwrap();
-    c.bench_function("promql", |b| {
-        b.to_async(&rt).iter(|| async {
-            let mut engine = newpromql::QueryEngine::new(Arc::clone(&ctx));
-            let data = engine.exec(black_box(eval_stmt.clone())).await.unwrap();
-            black_box(data);
-        })
-    });
+
+    let mut group = c.benchmark_group("promql");
+    group.measurement_time(Duration::from_secs(8));
+
+    for (alias, query) in [
+        (
+            "rate",
+            r#"sum by(endpoint) (rate(zo_http_incoming_requests{namespace="ziox-alpha1",organization="default"}[5m]))"#,
+        ),
+        (
+            "irate",
+            r#"sum by(endpoint) (irate(zo_http_incoming_requests{namespace="ziox-alpha1",organization="default"}[5m]))"#,
+        ),
+        (
+            "increase",
+            r#"sum by(endpoint) (increase(zo_http_incoming_requests{namespace="ziox-alpha1",organization="default"}[5m]))"#,
+        ),
+        (
+            "delta",
+            r#"sum by(stream_type) (delta(zo_storage_files{namespace="ziox-alpha1",organization="default"}[5m]))"#,
+        ),
+    ] {
+        let eval_stmt = parser::EvalStmt {
+            expr: parser::parse(query).unwrap(),
+            start: UNIX_EPOCH.checked_add(Duration::from_secs(start)).unwrap(),
+            end: UNIX_EPOCH.checked_add(Duration::from_secs(end)).unwrap(),
+            interval: Duration::from_secs(15), // step
+            lookback_delta: Duration::from_secs(300),
+        };
+        group.bench_function(BenchmarkId::from_parameter(format!("{alias}")), |b| {
+            b.to_async(&rt).iter(|| async {
+                let mut engine = newpromql::QueryEngine::new(Arc::clone(&ctx));
+                let data = engine.exec(black_box(eval_stmt.clone())).await.unwrap();
+                black_box(data);
+            })
+        });
+    }
+    group.finish();
 }
 
 criterion_group!(benches, bench_promql);
